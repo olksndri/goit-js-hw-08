72a8518e644d40b9eaa35d4a172b4773d5b32dbc
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-05-02 17:00:15 +0300

    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+
+const galleryItem = galleryItems.reduce((prevVal, elem) => {
+  prevVal += `<li class="gallery__item">
+   <a class="gallery__link" href="${elem.original}">
+      <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" title="${elem.description}" />
+   </a></li>`;
+  return prevVal;
+}, '');
+
+gallery.innerHTML = galleryItem;
+const options = {
+  captionSelector: 'img',
+  captionType: 'title',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+};
+
+let lightbox = new SimpleLightbox('.gallery a', options);
