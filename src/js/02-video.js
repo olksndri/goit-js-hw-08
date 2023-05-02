4077259a80823453c3f2e347ee62211787eb4173
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-05-02 22:33:52 +0300

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    save(LOCALSTORAGE_KEY, currentTime.seconds);
+    // console.log(currentTime);
+  }, 1000)
+);
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.setCurrentTime(load(LOCALSTORAGE_KEY));
