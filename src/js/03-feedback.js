04589757fd376bf38a9bddbe5a9dd3532cf79afd
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-05-04 16:01:35 +0300

    small code refactiring

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,42 +5,41 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input[name="email"]');
 const textarea = document.querySelector('textarea[name="message"]');
 const btn = document.querySelector('button[type="submit"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const STORAGE_KEY = 'feedback-form-state';
 let inpObj = { email: '', message: '' };
 
+checkingStorage();
+console.log('Received from storage: ', load(LOCALSTORAGE_KEY));
+
+btn.addEventListener('click', removeStorage);
 form.addEventListener(
   'input',
   throttle(function () {
     inpObj.email = input.value;
     inpObj.message = textarea.value;
-    save(STORAGE_KEY, inpObj);
-    console.log('input and storage changed, ', load(STORAGE_KEY));
+    save(LOCALSTORAGE_KEY, inpObj);
   }, 500)
 );
 
 function checkingStorage() {
-  if ((load(STORAGE_KEY).email = '')) {
+  if (load(LOCALSTORAGE_KEY).email === '') {
     input.value = '';
   } else {
-    input.value = load(STORAGE_KEY).email;
-    inpObj.email = load(STORAGE_KEY).email;
+    input.value = load(LOCALSTORAGE_KEY).email;
+    inpObj.email = load(LOCALSTORAGE_KEY).email;
   }
-  if ((load(STORAGE_KEY).message = '')) {
+  if (load(LOCALSTORAGE_KEY).message === '') {
     textarea.value = '';
   } else {
-    textarea.value = load(STORAGE_KEY).message;
-    inpObj.message = load(STORAGE_KEY).message;
+    textarea.value = load(LOCALSTORAGE_KEY).message;
+    inpObj.message = load(LOCALSTORAGE_KEY).message;
   }
 }
-
-checkingStorage();
-btn.addEventListener('click', removeStorage);
 
 function removeStorage() {
   inpObj = { email: '', message: '' };
-  save(STORAGE_KEY, inpObj);
+  save(LOCALSTORAGE_KEY, inpObj);
   input.value = '';
   textarea.value = '';
 }
-console.log('Received from storage: ', load(STORAGE_KEY));
